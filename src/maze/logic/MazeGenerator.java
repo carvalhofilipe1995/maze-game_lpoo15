c2b90aba5961898fb6efcfe0891442457793b36e
Luís Carvalho <carvalhofilipe1995@gmail.com>
2015-03-11 00:14:36 +0000

    labirintos aleatórios done!

--- src/maze/logic/MazeGenerator.java
+++ src/maze/logic/MazeGenerator.java
@@ -6,155 +6,283 @@ import java.awt.Point;
 
 public class MazeGenerator {
 	// camps
-	protected int sizex;
-	protected int sizey;
+	protected int height;
+	protected int width;
 	protected String[][] lab;
 	protected String[][] visitedCells;
 	protected int visitedCellsDimension;
 	protected Stack<Point> pathHistory;
+	protected Point guideCell;
+	protected Point exit;
 
 	// constructor
-	public MazeGenerator(int x, int y) {
-		this.sizex = x;
-		this.sizey = y;
-		this.lab = new String[50][50];
-		this.visitedCells = new String[30][30];
-		this.visitedCellsDimension = (sizex - 1) / 2;
+	public MazeGenerator(int height, int width) {
+		this.height = height;
+		this.width = width;
+		this.lab = new String[height][width];
+		this.visitedCells = new String[height - 2][width - 2];
+		this.visitedCellsDimension = (height - 1) / 2; // height == width
 		this.pathHistory = new Stack<Point>();
+		this.guideCell = new Point();
+		this.exit = new Point();
 	}
 
-	public int getX() {
-		return this.sizex;
+	public int getHeight() {
+		return this.height;
 	}
 
-	public int getY() {
-		return this.sizey;
+	public int getWidth() {
+		return this.width;
 	}
 
-	public void setMaze(int x, int y, String change) {
-		this.lab[x][y] = change;
-	}
-	
-	public void setVisitedCells(int x, int y, String change){
-		
+	public void setMaze(Point p, String change) {
+		this.lab[(int) p.getY()][(int) p.getX()] = change;
 	}
 
-	// Create map
-	public int createLab() {
+	public void setVisitedCells(Point p, String change) {
+		this.visitedCells[(int) p.getY()][(int) p.getX()] = change;
+	}
+
+	// Create Maze
+	public int createMaze() {
 		Random rand = new Random();
 
-		// fill everything with walls
-		for (int i = 0; i < sizex; i++) {
-			for (int j = 0; j < sizey; j++) {
-				setMaze(i, j, "X");
+		// fill everything with walls 'X' or ' '
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if ((i % 2) != 0 && (j % 2) != 0) {
+					Point p = new Point(j, i);
+					setMaze(p, " ");
+				} else {
+					Point change = new Point(j, i);
+					setMaze(change, "X");
+				}
 			}
 		}
-
-		// put the cells that have odd coordinates with " "
-		for (int i = 0; i < sizex - 1; i++) {
-			for (int j = 0; j < sizey - 1; j++) {
-				if ((i % 2) != 0 && (j % 2) != 0)
-					setMaze(i, j, " ");
-			}
-		}
-
-		generateExit();  // Randomly get the exit
-
-		// Visited Cells
 
 		// fill the visited cells array
 
-		for (int i = 0; i < visitedCellsDimension; i++) {
-			for (int j = 0; j < visitedCellsDimension; j++) {
+		for (int i = 0; i < visitedCells.length; i++) {
+			for (int j = 0; j < visitedCells.length; j++)
 				visitedCells[i][j] = ".";
-			}
 		}
 
-		int line = rand.nextInt(visitedCellsDimension - 1), collumn = rand
-				.nextInt(visitedCellsDimension - 1);
+		generateExit(); // Randomly get the exit
 
-		visitedCells[line][collumn] = "+";
+		startGuideCell(); // Initialize Guide cell
 
-		Point guideCell = new Point(line, collumn);
-		pathHistory.push(guideCell);
-		
+		setVisitedCells(guideCell, "+");
 
+		pathHistory.push(new Point((int) guideCell.getX(), (int) guideCell
+				.getY()));
+
+		do {
+
+			while (!checkMovements()) {
+				pathHistory.pop();
+				if (pathHistory.empty())
+					break;
+				else
+					guideCell = pathHistory.peek();
+			}
+
+			if (pathHistory.empty())
+				break;
+
+			int direction;
+			do {
+				direction = rand.nextInt(4);
+			} while (!checkIfMoveIsValid(direction));
+
+			switch (direction) {
+			case 0: // up
+				Point change_up = new Point((int) guideCell.getX() * 2 + 1,
+						(int) guideCell.getY() * 2);
+				setMaze(change_up, " ");
+				break;
+			case 1: // down
+				Point change_down = new Point((int) guideCell.getX() * 2 + 1,
+						(int) (guideCell.getY() + 1) * 2);
+				setMaze(change_down, " ");
+				break;
+			case 2: // right
+				Point change_right = new Point(
+						(int) (guideCell.getX() + 1) * 2,
+						(int) guideCell.getY() * 2 + 1);
+				setMaze(change_right, " ");
+				break;
+			case 3: // left
+				Point change_left = new Point((int) guideCell.getX() * 2,
+						(int) guideCell.getY() * 2 + 1);
+				setMaze(change_left, " ");
+				break;
+			default:
+				break;
+			}
+
+			updateGuideCell(direction);
+			// Point p = guideCell;
+			pathHistory.push(new Point((int) guideCell.getX(), (int) guideCell
+					.getY()));
+
+		} while (pathHistory.empty() == false);
 
 		return 0;
 	}
 
-	public int generateExit() {
+	// Done
+	public void generateExit() {
 		Random rand = new Random();
-		int r = rand.nextInt(3);
-		if (r == 0) {
-			// collumn = 0
-			int line = rand.nextInt(sizex - 1);
-			setLab(line, 0, "S");
 
-		} else if (r == 1) {
-			// line = 0
-			int collumn = rand.nextInt(sizey - 1);
-			setLab(0, collumn, "S");
+		int exitZ;
+		do {
+			exitZ = rand.nextInt(lab.length - 2) + 1;
+		} while (exitZ % 2 == 0);
 
-		} else if (r == 2) {
-			// collumn = sizey - 1
-			int line = rand.nextInt(sizex - 1);
-			setLab(line, sizey - 1, "S");
-
-		} else if (r == 3) {
-			// line = sizex - 1
-			int collumn = rand.nextInt(sizey - 1);
-			setLab(sizex - 1, collumn, "S");
+		// getting random border to place exit at
+		switch (rand.nextInt(4)) {
+		case 0: // top
+			exit.setLocation(exitZ, exit.getY());
+			break;
+		case 1: // right
+			exit.setLocation(lab.length - 1, exitZ);
+			break;
+		case 2: // bottom
+			exit.setLocation(exitZ, lab.length - 1);
+			break;
+		case 3: // left
+			exit.setLocation(exit.getX(), exitZ);
+			break;
+		default:
+			exit.setLocation(1, exit.getY());
+			break;
 		}
-		return 0;
+
+		// exit
+		lab[(int) exit.getY()][(int) exit.getX()] = "S";
 	}
 
-	public boolean checkVisitedCells(int x, int y) {
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
-				if (visitedCells[i][j] != "+")
-					return false;
-			}
-		}
-		return true;
+	// Done
+	public void startGuideCell() {
+		Point nearExit = new Point((int) exit.getX(), (int) exit.getY());
+
+		if (exit.getX() == 0) {
+			nearExit.setLocation(nearExit.getX() + 1, nearExit.getY());
+		} else if (exit.getX() == lab.length - 1) {
+			nearExit.setLocation(nearExit.getX() - 1, nearExit.getY());
+		} else if (exit.getY() == 0) {
+			nearExit.setLocation(nearExit.getX(), nearExit.getY() + 1);
+		} else if (exit.getY() == lab.length - 1)
+			nearExit.setLocation(nearExit.getX(), nearExit.getY() - 1);
+
+		int guideCellX = ((int) nearExit.getX() - 1) / 2;
+		int guideCellY = ((int) nearExit.getY() - 1) / 2;
+
+		guideCell.setLocation(guideCellX, guideCellY);
 	}
 
-	public boolean checkIfCantMove(int direction, int line, int collumn) {
-		if (direction == 0) {
-			if (collumn + 1 >= visitedCellsDimension) {
-				return true;
-			}
-		} else if (direction == 1) {
-			if (collumn - 1 < 0) {
-				return true;
-			}
-		} else if (direction == 2) {
-			if (line + 1 >= visitedCellsDimension) {
-				return true;
-			}
-		} else if (direction == 3) {
-			if (line - 1 < 0) {
+	// Done
+	public boolean checkMovements() {
+		for (int i = 0; i < 4; i++) {
+			if (checkIfMoveIsValid(i) == true) {
 				return true;
 			}
 		}
 		return false;
 	}
 
+	// Done
+	public boolean cellHasBeenVisited(int direction) {
+		switch (direction) {
+		case 0: // up
+			if (visitedCells[(int) guideCell.getY() - 1][(int) guideCell.getX()] == "+")
+				return true;
+			break;
+		case 1: // down
+			if (visitedCells[(int) guideCell.getY() + 1][(int) guideCell.getX()] == "+")
+				return true;
+			break;
+		case 2: // right
+			if (visitedCells[(int) guideCell.getY()][(int) guideCell.getX() + 1] == "+")
+				return true;
+			break;
+		case 3: // left
+			if (visitedCells[(int) guideCell.getY()][(int) guideCell.getX() - 1] == "+")
+				return true;
+			break;
+		default:
+			break;
+		}
+		return false;
+	}
+
+	// Done
+	public boolean checkIfMoveIsValid(int direction) {
+		switch (direction) {
+		case 0: // up
+			if (guideCell.getY() - 1 < 0) {
+				return false;
+			}
+			break;
+		case 1: // down
+			if (guideCell.getY() + 1 >= visitedCellsDimension) {
+				return false;
+			}
+			break;
+		case 2: // right
+			if (guideCell.getX() + 1 >= visitedCellsDimension) {
+				return false;
+			}
+			break;
+		case 3: // left
+			if (guideCell.getX() - 1 < 0) {
+				return false;
+			}
+			break;
+		default:
+			break;
+		}
+		return !cellHasBeenVisited(direction);
+	}
+
+	// Done
+	public void updateGuideCell(int direction) {
+		switch (direction) {
+		case 0: // up
+			guideCell.setLocation(guideCell.getX(), guideCell.getY() - 1);
+			setVisitedCells(guideCell, "+");
+			break;
+		case 1: // down
+			guideCell.setLocation(guideCell.getX(), guideCell.getY() + 1);
+			setVisitedCells(guideCell, "+");
+			break;
+		case 2: // right
+			guideCell.setLocation(guideCell.getX() + 1, guideCell.getY());
+			setVisitedCells(guideCell, "+");
+			break;
+		case 3: // left
+			guideCell.setLocation(guideCell.getX() - 1, guideCell.getY());
+			setVisitedCells(guideCell, "+");
+			break;
+		default:
+			break;
+		}
+	}
+
 	// Print map
 	public void printLabirinto() {
-		for (int i = 0; i < getX(); i++) {
-			for (int j = 0; j < getY(); j++) {
-				System.out.print(this.lab[i][j]);
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				System.out.print(this.lab[i][j] + ' ');
 			}
 			System.out.print("\n");
 		}
 	}
 
 	public static void main(String args[]) {
-		MazeGenerator maze = new MazeGenerator(11, 11);
-		maze.createLab();
+		MazeGenerator maze = new MazeGenerator(21,21);
+		maze.createMaze();
 		maze.printLabirinto();
 	}
-
 
 }
