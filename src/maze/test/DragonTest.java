83b09c96ac98272bd6dc7d3f0a0e7fa197a5a1da
Luís Carvalho <carvalhofilipe1995@gmail.com>
2015-04-03 00:33:44 +0100

    TESTES - só inicio

--- src/maze/test/DragonTest.java
+++ src/maze/test/DragonTest.java
@@ -1,5 +1,30 @@
 package maze.test;
 
+import maze.logic.*;
+
+import org.junit.Assert.*;
+import org.junit.Test;
+
 public class DragonTest {
 
+	@Test
+	public void validPositions(){
+		
+	}
+	
+	@Test
+	public void dragonMoves(){
+		
+	}
+	
+	@Test
+	public void dragonSleeping(){
+		
+	}
+	
+	@Test
+	public void dragonKillByHero(){
+		
+	}
+	
 }
--- src/maze/test/HeroTest.java
+++ src/maze/test/HeroTest.java
@@ -1,5 +1,35 @@
 package maze.test;
 
+import maze.logic.*;
+
+import org.junit.Assert.*;
+import org.junit.Test;
+
 public class HeroTest {
+	
+	@Test
+	public void validPositions(){
+		
+	}
+	
+	@Test
+	public void getSword(){
+		
+	}
+	
+	@Test
+	public void heroKillByDragon(){
+		
+	}
+	
+	@Test
+	public void heroKillDragon(){
+		
+	}
+	
+	@Test
+	public void heroWins(){
+		
+	}
 
 }
